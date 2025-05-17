b09f81b1513f5b4e6f74f14cb827b37c9f850d98
LennyMX <patron.luis@uabc.edu.mx>
2025-05-17 13:49:26 -0700

    Carta.java

--- Carta.java
+++ Carta.java
@@ -1,10 +1,16 @@
 public class Carta {
     private String valor;
     private String palo;
+    private boolean visible; 
 
     public Carta(String valor, String palo) {
+        this(valor, palo, true); 
+    }
+
+    public Carta(String valor, String palo, boolean visible) {
         this.valor = valor;
         this.palo = palo;
+        this.visible = visible;
     }
 
     public String getValor() {
@@ -15,9 +21,17 @@ public class Carta {
         return palo;
     }
 
+    public boolean esVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
     @Override
     public String toString() {
-        return valor + " de " + palo;
+        return (visible ? valor + " de " + palo : "Carta Oculta");
     }
 }
 
