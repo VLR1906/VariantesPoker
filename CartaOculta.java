c9ae2b2c2ac658dbb32ed7bf395e3a247473007a
LennyMX <patron.luis@uabc.edu.mx>
2025-05-20 01:38:18 -0700

    CartaOculta.java

--- CartaOculta.java
+++ CartaOculta.java
@@ -1,6 +1,13 @@
 public class CartaOculta extends Carta {
+    private Carta original;
+
     public CartaOculta(Carta original) {
         super(original.getValor(), original.getPalo());
+        this.original = original;
+    }
+
+    public Carta getOriginal() {
+        return original;
     }
 
     @Override
@@ -12,4 +19,5 @@ public class CartaOculta extends Carta {
     public String toString() {
         return "[Carta oculta]";
     }
+
 }
